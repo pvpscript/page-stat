54bf6c472c34232f9b7698a895baf08d2c800d99
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-06-12 15:49:41 -0300

    Changed button style and text

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -2,5 +2,4 @@ let changeColor = document.getElementById('changeColor');
 
 chrome.storage.sync.get('color', function(data) {
 	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', 'This is a non blacklisted page');
 });
