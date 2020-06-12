922afe6c3534658fde8a71115c7436ebc0a9d7d0
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-06-12 15:42:02 -0300

    Removed default popup files

--- src/chrome/popup.js
+++
@@ -1,6 +0,0 @@
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data) {
-	changeColor.style.backgroundColor = data.color;
-	changeColor.setAttribute('value', data.color);
-});
