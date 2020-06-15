f8c46f3d9575f0a3dd99d8379a850202b2e01ac2
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-06-14 23:48:04 -0300

    Minor fixes

--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -1,22 +1,22 @@
 const hostname = document.getElementById("hostname");
-const hoststatus = document.getElementById("hoststatus");
 const cbSwitch = document.getElementById("switch-shadow");
 
 hostname.innerText = "developer.fourhourworkweek.com";
 hostname.style.color = "#06758F";
 
-hoststatus.innerText = "ACTIVE";
 //hoststatus.style.color = "#8F0B00";
-hoststatus.style.color = "#4A8F06";
+//hoststatus.style.color = "#4A8F06";
 
 cbSwitch.addEventListener('change', (e) => {
-	if (e.target.checked) {
+  if (e.target.checked) {
   	console.log("On");
   } else {
   	console.log("Off");
   }
 });
 
+/*
 chrome.storage.sync.get('color', function(data) {
 	changeColor.style.backgroundColor = data.color;
 });
+*/
