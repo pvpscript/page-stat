e864bacd692d5b4e0c9027c1d1ad99f2a50c108a
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-06-17 22:02:59 -0300

    Major rewrite to use message communication between content script and extension

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,106 +1,94 @@
-function tabAction(tabId, changeInfo, tab) {
+function changeIcon(tabId, iconPath) {
+	chrome.browserAction.setIcon({
+		tabId: tabId,
+		path: iconPath
+	}, () => {
+		console.log("Icon has been changed");
+	});
+}
+
+const methods = {
+	handshake: async (tab, callback, data) => {
+		const url = new URL(tab.url);
+		const pageStat = await checkStatus(url.host);
+
+		callback({
+			host: url.host,
+			pageStat: pageStat,
+			time: 0,
+		});
+	},
+	change: async (tab, callback, data) => {
+		const url = new URL(tab.url);
+		chrome.storage.sync.get('inactive', (obj) => {
+			if (data.deactivate) {
+				obj.inactive.push(url.host);
+				changeIcon(tab.id, "images/test.png");
+			} else {
+				const index = obj.inactive.indexOf(url.host);
+				obj.inactive.splice(index, 1);
+				changeIcon(tab.id, "images/graph_icon128.png");
+			}
+			chrome.storage.sync.set({inactive: obj.inactive});
+		});
+
+		callback({});
+	},
+}
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	const action = methods[request.type];
+
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		action(tabs[0], sendResponse, request.data);
+	});
+
+	return true;
+});
+
+async function checkStatus(host) {
+	return new Promise((resolve, reject) => {
+		try {
+			chrome.storage.sync.get('inactive', (data) => {
+				resolve(!data.inactive.includes(host));
+			});
+		} catch (ex) {
+			reject(ex);
+		}
+	});
+}
+
+async function tabAction(tab) {
 	if (tab.url) {
 		const url = new URL(tab.url);
-
-		checkBlacklist(tabId, url.host);
 
 		if (
 			url.protocol == "about:" ||
 			url.protocol == "chrome:"
 		) {
-			chrome.browserAction.disable(tabId);
+			chrome.browserAction.disable(tab.id);
+		} else {
+			const pageStat = await checkStatus(url.host);
+			changeIcon(tab.id, pageStat
+				? "images/graph_icon128.png"
+				: "images/test.png");
 		}
-		chrome.storage.sync.set({host: url.host});
 	}
 }
 
-
-function checkBlacklist(tabId, host) {
-	chrome.storage.sync.get('inactive', (data) => {	
-		if (data.inactive.includes(host)) {
-			chrome.storage.sync.set({inactivated: true}); // stores inactivated site flag
-			chrome.browserAction.setIcon({
-				tabId: tabId,
-				path:"images/test.png"
-			}, () => {
-				console.log("Icon has been changed");
-			});
-		} else {
-			chrome.storage.sync.set({inactivated: false});
-		}
-	});
-}
-
-chrome.tabs.query({}, (tabs) => {
-	console.log(tabs);
-});
-
-chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-	console.log(`Tab with id "${tabId}" was closed!`);
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	tabAction(tabId, changeInfo, tab);
+	tabAction(tab);
 });
 
 chrome.tabs.onActivated.addListener((activeInfo) => {
 	chrome.tabs.get(activeInfo.tabId, (tab) => {
-		tabAction(activeInfo.tabId, null, tab);
+		tabAction(tab);
 	});
 });
 
-const methods = {
-	'ban': (host) => {
-		chrome.storage.sync.get('inactive', (data) => {
-			data.inactive.push(host);
-			console.log(data.inactive);
-			chrome.storage.sync.set({inactive: data.inactive});
-		});
-	},
-	'unban': (host) => {
-		chrome.storage.sync.get('inactive', (data) => {
-			const index = data.inactive.indexOf(host);
-			data.inactive.splice(index, 1);
-			console.log(data.inactive);
-			chrome.storage.sync.set({inactive: data.inactive});
-		});
-	}
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	const action = methods[request.call];
-
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-		const url = new URL(tabs[0].url);
-		action(url.host);
-
-		tabAction(tabs[0].id, null, tabs[0]);
-	});
-
-	console.log(`Request ${JSON.stringify(request)}`);
-	console.log(`Sender ${JSON.stringify(sender)}`);
-});
-
-chrome.runtime.onInstalled.addListener(function(message, sender, sendResponse) {
+chrome.runtime.onInstalled.addListener((message, sender, sendResponse) => {
 	chrome.storage.sync.set({
 		inactive: [],
 	}, () => {
 		console.log("Local data set as empty.");
 	});
-
-	/*chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([
-			blacklistRule
-		]);
-	});*/
-	/*chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'developer.chrome.com'},
-			})
-			],
-				actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
-	*/
 });
--- src/chrome/popup.js
+++ src/chrome/popup.js
@@ -1,43 +1,33 @@
 const hostname = document.getElementById("hostname");
 const cbSwitch = document.getElementById("switch-shadow");
-const spent = document.getElementById("spent");
+const spentSection = document.getElementById("spent");
+const spentTime = document.getElementById("spent-time");
 
 cbSwitch.addEventListener('change', (e) => {
-	/*
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-		chrome.tabs.sendMessage(tabs[0].id, {
-			call: e.target.checked ? 'unban' : 'ban',
-		});
-	});
-	*/
 	chrome.runtime.sendMessage({
-		call: e.target.checked ? 'unban' : 'ban',
+		type: 'change',
+		data: {deactivate: !e.target.checked}
+	}, (response) => {
 	});
 });
 
-const methods = {
-	'inactivated': () => {},
-	'activated': () => {},
+chrome.runtime.sendMessage({type: 'handshake', data: null}, (response) => {
+	hostname.innerText = response.host;
+	cbSwitch.checked = response.pageStat;
+	if (response.pageStat) {
+		spentSection.style.display = 'inline';
+		spentTime.innerText = getFormattedTime(response.time);
+	} else {
+		spentSection.style.display = 'none';
+	}
+});
+
+function getFormattedTime(time) {
+	const hours = Math.floor(time / 3600);
+	const mins = Math.floor((time - hours * 3600) / 60);
+	const secs = time - hours * 3600 - mins * 60;
+
+	return ("0"+hours).slice(-2) +
+		":" + ("0"+mins).slice(-2) +
+		":" + ("0"+secs).slice(-2);
 }
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-});
-
-chrome.storage.sync.get('host', (data) => {
-	hostname.innerText = data.host;
-});
-
-chrome.storage.sync.get('inactivated', (data) => {
-	if (!data.inactivated) {
-	spent.style.display = 'inline';
-	//cbSwitch.checked = true;
-} else {
-	cbSwitch.checked = false;
-}
-});
-
-/*
-chrome.storage.sync.get('color', function(data) {
-	changeColor.style.backgroundColor = data.color;
-});
-*/
