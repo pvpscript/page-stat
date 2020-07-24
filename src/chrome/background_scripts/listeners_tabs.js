5dfcabe68624f0a6c71f5e3b89b8308f22729215
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-24 19:51:29 -0300

    Displaying URL

--- src/chrome/background_scripts/listeners_tabs.js
+++ src/chrome/background_scripts/listeners_tabs.js
@@ -1,24 +1,24 @@
 chrome.tabs.onActivated.addListener((activeInfo) => {
 	chrome.tabs.get(activeInfo.tabId, (tab) => {
-		alert("Tab just got activated");
+		const url = buildURL(tab.url);
 
-		console.log("Tab just got activated");
+		console.log(`Tab just got activated: ${url}`);
 		console.log(tab);
 	});
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	alert("Tab just got updated");
+	const url = buildURL(tab.url);
 
-	console.log("Tab just got updated");
+	console.log(`Tab just got updated: ${url}`);
 	console.log(tab);
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 	chrome.tabs.get(tabId, (tab) => { 
-		alert("Tab just got removed");
+		const url = buildURL(tab.url);
 
-		console.log("Tab just got removed");
+		console.log(`Tab just got removed: ${url}`);
 		console.log(tab);
 	});
 });
