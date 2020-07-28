57dc1f9b351b486b2c91b06b07ea63fab1579b95
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-28 10:41:15 -0300

    Pages time update working

--- src/chrome/background_scripts/listeners_tabs.js
+++ src/chrome/background_scripts/listeners_tabs.js
@@ -1,23 +1,94 @@
-chrome.tabs.onActivated.addListener((activeInfo) => {
-	chrome.tabs.get(activeInfo.tabId, (tab) => {
-		const url = buildURL(tab.url);
+let pagesCache = {}; /* Stored pages cache */
 
-		console.log(`Tab just got activated: ${url}`);
-		console.log(tab);
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	chrome.storage.sync.get(['pages'], (res) => {
+		pagesCache = res.pages;
+
+		chrome.tabs.get(activeInfo.tabId, (tab) => {
+			const url = buildURL(tab.url);
+
+			/*
+			console.log("-------------------- Before --------------------");
+			console.log(pagesCache);
+			console.log("------------------------------------------------");
+			*/
+
+			if (url) {
+				console.log(`Tab just got activated: ${url}`);
+				console.log(tab);
+
+				updateFocus(tab.windowId, url.host);
+			}
+
+			/*
+			console.log("-------------------- After --------------------");
+			console.log(pagesCache);
+			chrome.storage.sync.get(['pages'], (res) => console.log(res.pages));
+			console.log("-----------------------------------------------");
+			*/
+			chrome.storage.sync.set({pages: pagesCache});
+			/*
+			chrome.storage.sync.get(['pages'], (res) => console.log(res.pages));
+			*/
+			
+		});
 	});
 });
 
-let lastHost = null; /* Used to avoid multiple updates to the same host */
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-	const url = buildURL(tab.url);
+	chrome.storage.sync.get(['pages'], (res) => {
+		pagesCache = res.pages;
 
-	if (url && url.host != lastHost) {
-		lastHost = url.host;
-		console.log(`Tab just got updated: ${url}`);
-		console.log(tab);
-	}
+		/*
+		console.log("-------------------- Before --------------------");
+		console.log(pagesCache);
+		console.log("------------------------------------------------");
+		*/
+
+		const url = buildURL(tab.url);
+
+		if (url) {
+			console.log(`Tab just got updated: ${url}`);
+			console.log(tab);
+
+			updateFocus(tab.windowId, url.host);
+		}
+		/*console.log("-------------------- After --------------------");
+		console.log(pagesCache);
+		chrome.storage.sync.get(['pages'], (res) => console.log(res.pages));
+		console.log("-----------------------------------------------");
+		*/
+		chrome.storage.sync.set({pages: pagesCache});
+		/*
+		chrome.storage.sync.get(['pages'], (res) => console.log(res.pages));
+		*/
+	});
 });
 
 chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
 	console.log(`Tab just got removed: ${tabId}`);
 });
+
+function updatePagesCache(callback) {
+	chrome.storage.sync.get(['pages'], (res) => {
+		pagesCache = res.pages;
+
+		console.log("-------------------- Before --------------------");
+		console.log(pagesCache);
+		console.log("------------------------------------------------");
+		callback(pagesCache);
+		//res.pages[Math.floor(Math.random()*10)] = Math.random().toString(36);
+		console.log("-------------------- After --------------------");
+		console.log(pagesCache);
+		console.log("-----------------------------------------------");
+
+		chrome.storage.sync.set({pages: pagesCache});
+		chrome.storage.sync.get(['pages'], (kkj) => {
+			console.log("CHEIRO DE LEEMAUN");
+			console.log(kkj.pages)
+			console.log("CHEIRO DE LEEMAUN");
+			console.log(pagesCache);
+			console.log("AGUINHA");
+		});
+	});
+}
