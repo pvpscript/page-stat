22eb432ec618c80bace1e2739b774712dd098155
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-29 00:50:11 -0300

    Inactive hosts changes url stat

--- src/chrome/background_scripts/utils.js
+++ src/chrome/background_scripts/utils.js
@@ -2,10 +2,12 @@ function buildURL(url) {
 	try {
 		const urlObj = new URL(url);
 		const protocol = urlObj.protocol.split(":")[0];
+		const stat = config.protocols.includes(protocol) &&
+			!config.inactive[urlObj.host];
 
 		return {
 			url: urlObj,
-			stat: config.protocols.includes(protocol)
+			stat: stat
 		}
 	} catch (e) {
 	}
