dec7c7b100b01cc42ea31feb388702eb74a9c3ff
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-07-28 23:13:07 -0300

    Added favicon fetching with google API

--- src/chrome/background_scripts/page.js
+++ src/chrome/background_scripts/page.js
@@ -1,6 +1,21 @@
+const faviconAPI = "https://www.google.com/s2/favicons?domain=";
+
 class Page {
 	constructor(host) {
-		this.time = Date.now() - host.focusedAt;
-		this.favicon = null;
+		return (async () => {
+			this.time = Date.now() - host.focusedAt;
+			this.favicon = await fetch(faviconAPI + host.host)
+				.then(res => res.blob())
+				.then(blob => new Promise((resolve, reject) => {
+					const reader = new FileReader();
+					reader.onloadend = () => {
+						resolve(reader.result);
+					}
+					reader.onerror = reject;
+					reader.readAsDataURL(blob);
+				}));
+
+			return this;
+		})();
 	}
 }
