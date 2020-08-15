ad970acf33f4de16e435806259a2344d88a48911
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2020-08-14 21:33:40 -0300

    Hosts sorted by usage time

--- src/view/statistics/index.js
+++ src/view/statistics/index.js
@@ -2,21 +2,31 @@ const sites = document.getElementById("sites");
 
 chrome.storage.local.get(['pages'], (res) => {
 	const pages = res.pages;
-	let index = 1;
+	const hostTime = [];
 
-	Object.keys(pages).sort().forEach((h) => {
+	for (let h in pages) {
+		let totalTime = 0;
+		for (let t in pages[h].time) {
+			totalTime += pages[h].time[t];
+		}
+		
+		hostTime.push([h, totalTime]);
+	}
+
+	console.log(hostTime);
+
+	let index = 1;
+	hostTime.sort((a, b) => { return b[1] - a[1]; }).forEach((e) => {
+	  const h = e[0];
+	  const time = e[1];
+
 	  row = sites.insertRow();
 	  cell = row.insertCell();
 	  cell.appendChild(document.createTextNode(index++));
 	  cell2 = row.insertCell();
 	  cell2.appendChild(document.createTextNode(h));
 	  cell3 = row.insertCell();
-	  
-	  let totalTime = 0;
-	  for(let i in pages[h].time) {
-		totalTime += pages[h].time[i];
-	  }
 
-	  cell3.appendChild(document.createTextNode(totalTime));
+	  cell3.appendChild(document.createTextNode(time));
 	});
 });
